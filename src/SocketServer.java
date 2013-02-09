a37321a3b6715f44901cd9c22e35a162b3b93385
James Reinholdt <jjreinholdt@gmail.com>
2013-02-09 15:27:35 -0600

    Adding src

---
+++ src/SocketServer.java
@@ -0,0 +1,45 @@
+/*Author: James Reinholdt
+Date: 1/26/2013
+---------------------------------*/
+import java.net.*;
+import java.io.*;
+import java.util.*;
+
+public class SocketServer{
+	public static Queue<String> tasks=new LinkedList<String>();
+	
+	public static void main(String[] args) throws IOException {
+		ServerSocket serverSocket = null;
+		try {
+			serverSocket = new ServerSocket(4444);
+		} catch (IOException e) {
+			System.err.println("Could not listen on port: 4444.");
+			System.exit(1);
+		}
+		
+		Socket client = null;
+		try {
+			client = serverSocket.accept();
+		} catch(IOException e) {
+			System.err.println("Accept failed.");
+			System.exit(1);
+		}
+		
+		PrintWriter out=new PrintWriter(client.getOutputStream(), true);
+		BufferedReader in=new BufferedReader(new InputStreamReader(client.getInputStream()));
+		
+		String input,output;
+		
+		while ((input = in.readLine())!=null) {
+			tasks.add(input);
+			out.println(tasks.poll());
+			if(input.equals("DISCONNECT"))
+				break;
+        }
+		
+		out.close();
+        in.close();
+        client.close();
+        serverSocket.close();
+	}
+}
---
+++ src/TestGIT.java
@@ -0,0 +1,12 @@
+
+public class TestGIT {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
